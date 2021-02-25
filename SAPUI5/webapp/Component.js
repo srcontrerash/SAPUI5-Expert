605c84dbc83f6d451ab88cc753a351b3974c1f29
srcontrerash <scontrerash@gmail.com>
2021-02-25 03:23:33 +0000

    05 Estructura Proyectos SAPUI5

---
+++ SAPUI5/webapp/Component.js
@@ -0,0 +1,29 @@
+// @ts-nocheck
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "logaligroup/SAPUI5/model/Models",
+    "sap/ui/model/resource/ResourceModel"
+
+],
+    /**
+    * @param {typeof sap.ui.core.UIComponent} UIComponent
+    * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
+    */
+    function (UIComponent, Models, ResourceModel) {
+        return UIComponent.extend("logaligroup.SAPUI5.Component", {
+            metadata: {
+                manifest: "json"   
+            },
+            init: function () {
+                //call the init function of the parent
+                UIComponent.prototype.init.apply(this, arguments);
+
+                // set data model on the view
+                this.setModel(Models.createRecipient());
+                
+                //set i18n model on the view
+                var i18nModel = new ResourceModel({ bundleName: "logaligroup.SAPUI5.i18n.i18n" });
+                this.setModel(i18nModel, "i18n");
+            }
+        });
+    });
